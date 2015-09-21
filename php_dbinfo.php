985a9fa4f7832d49e3841f855648637f7518ef05
jorgemauricio <jorge.mauricio@me.com>
2015-09-21 10:21:33 -0500

    php_dbinfo.php: agregar los metodos de verificación de la base de datos index.php implementación del metodo para desplegar las imagenes del carrusel mediante la base de datos

--- php_dbinfo.php
+++ php_dbinfo.php
@@ -1,5 +1,16 @@
 <?php
 	$username="root";
-	$password="root";
-	$database="usuarios";
+	$password="";
+	$database="lnmysr";
+
+	$connection=mysql_connect ('localhost', $username, $password);
+	if (!$connection) {
+    	die('Not connected : ' . mysql_error());
+	}
+
+	// Set the active MySQL database
+	$db_selected = mysql_select_db($database, $connection);
+	if (!$db_selected) {
+	  die ('Can\'t use db : ' . mysql_error());
+	}
 ?>
