cba844ad621e0e256c8108278571f21e71f24596
jorgemauricio <jorge.mauricio@me.com>
2015-09-24 12:51:51 -0500

    método para subir las notas completo, faltando checar la validación de datos.

--- uploadNota.php
+++ uploadNota.php
@@ -17,18 +17,17 @@
     if(isset($_POST["submit"])) {
         $titulo = $_POST['titulo'];
         $portada =$_POST['portada'];
-        $linkimagen = $_POST['linkimagen'];
-        $linkpdf = $_POST['linkpdf'];
-        $infotext = $_POST['infotext'];
+        
         // Path to save the PDF
         $target_dir_imagen = "documentos/notas/";
         $target_dir_pdf =  "documentos/notas/";
         // Complete path
-        $target_file_imagen = $target_dir . basename($_FILES["archivoimagen"]["name"]);
-        $target_file_pdf = $target_dir . basename($_FILES["archivopdf"]["name"]);
+        $target_file_imagen = $target_dir_imagen . basename($_FILES["archivoimagen"]["name"]);
+        $target_file_pdf = $target_dir_imagen . basename($_FILES["archivopdf"]["name"]);
         $linkimagen = basename($_FILES["archivoimagen"]["name"]);
         $linkpdf = basename($_FILES["archivopdf"]["name"]);
-        $uploadOk = 1;
+        $uploadOk_image = 1;
+        $uploadOk_pdf = 1;
         $FileType_image = pathinfo($target_file_imagen,PATHINFO_EXTENSION);
         $FileType_pdf = pathinfo($target_file_pdf,PATHINFO_EXTENSION);
         
@@ -36,77 +35,54 @@
         // Check if file already exists
         if (file_exists($target_file_imagen)) {
             echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Error:</strong> El archivo ya existe</div>';
+            <strong>Error:</strong> La imagen ya existe</div>';
             $uploadOk_image = 0;
         }
         
         // Allow certain file formats
         if($FileType_image != "png") {
             echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Error:</strong> Solo puedes subir archivos con extensión .pdf</div>';
+            <strong>Error:</strong> Solo puedes subir imagenes con extensión .pdf</div>';
             $uploadOk_image = 0;
-        }
-        
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk_image == 0) {
-            echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Error:</strong> El archivo no fue subido al sistema.</div>';
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["archivo"]["tmp_name"], $target_file)) {
-                $query = "INSERT INTO casoestudio (titulo, autor, poptitulo, infotext, link) VALUES ('".$titulo."','".$autor."','".$poptitulo."','".$infotext."','".$link."')";
-                $result = mysql_query($query);
-                // Validate insert in the sql db
-                if (!$result) {
-                    die('Invalid query: ' . mysql_error());
-                }else{
-                    echo '<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    <strong>Archivo subido correctamente</strong></div>';
-                }      
-            } else {
-                echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                <strong>Error al momento de subir el archivo, intenta de nuevo</strong></div>';
-            }
         }
 
         // Check for PDF //
         // Check if file already exists
-        if (file_exists($target_file_imagen)) {
+        if (file_exists($target_file_pdf)) {
             echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
             <strong>Error:</strong> El archivo ya existe</div>';
-            $uploadOk_image = 0;
+            $uploadOk_pdf = 0;
         }
         
         // Allow certain file formats
-        if($FileType_image != "png") {
+        if($FileType_pdf != "pdf") {
             echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
             <strong>Error:</strong> Solo puedes subier archivos con extensión .pdf</div>';
-            $uploadOk_image = 0;
+            $uploadOk_pdf = 0;
         }
         
         // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
+        if (($uploadOk_image == 0) && ($uploadOk_pdf == 0)) {
             echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Error:</strong> El archivo no fue subido al sistema.</div>';
+            <strong>Error:</strong> La imagen y el PDF no fueron subidos al sistema.</div>';
         // if everything is ok, try to upload file
         } else {
-            if (move_uploaded_file($_FILES["archivo"]["tmp_name"], $target_file)) {
-                $query = "INSERT INTO casoestudio (titulo, autor, poptitulo, infotext, link) VALUES ('".$titulo."','".$autor."','".$poptitulo."','".$infotext."','".$link."')";
+            if (move_uploaded_file($_FILES["archivoimagen"]["tmp_name"], $target_file_imagen) && move_uploaded_file($_FILES["archivopdf"]["tmp_name"], $target_file_pdf)) {
+                $query = "INSERT INTO notasportada (titulo, linkpdf, linkimagen , portada) VALUES ('".$titulo."','".$linkpdf."','".$linkimagen."','".$portada."')";
                 $result = mysql_query($query);
                 // Validate insert in the sql db
                 if (!$result) {
                     die('Invalid query: ' . mysql_error());
                 }else{
                     echo '<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    <strong>Archivo subido correctamente</strong></div>';
+                    <strong>Imagen y PDF almacenados correctamente</strong></div>';
                 }      
             } else {
                 echo '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                <strong>Error al momento de subir el archivo, intenta de nuevo</strong></div>';
+                <strong>Error al momento de subir la imagen y el PDF, intenta de nuevo</strong></div>';
             }
         }
     }
-    
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -122,31 +98,30 @@
     <?php include("/includes/header.html");?>
 
     <body>
-        <h1 class="text-center">Subir Caso de Estudio</h1>
+        <h1 class="text-center">Subir Nota</h1>
         <br>
         <div class="container-fluid">
             <div class="container">
                 <div class="col-sm-6">
-                    <form class="form-horizontal" role="form" action="uploadCaso.php" method="post" enctype="multipart/form-data">
+                    <form class="form-horizontal" role="form" action="uploadNota.php" method="post" enctype="multipart/form-data">
                         <div class="form-group"> 
                             <label for="titulo">Título: </label>
                             <input type="text" class="form-control" name="titulo">
                         </div>
                         <div class="form-group"> 
-                            <label for="autor">Autor(es): </label>
-                            <input type="text" class="form-control" name="autor">
-                        </div>
-                        <div class="form-group"> 
-                            <label for="poptitulo">Tipo de Texto Informativo: </label>
-                            <input type="text" class="form-control" name="poptitulo">
-                        </div>
-                        <div class="form-group"> 
-                            <label for="infotext">Texto Informativo: </label>
-                            <textarea type="text" class="form-control" name="infotext"></textarea>
+                            <label for="portada">Portada: </label>
+                            <select class="form-control" name="portada">
+                            <option>NO</option>
+                            <option>SI</option>
+                        </select>
                         </div>
                         <div class="form-group">
-                            <label for="archivo">Archivo: </label>
-                            <input type="file" class="form-control" name="archivo">
+                            <label for="archivoimagen">Archivo Imagen: </label>
+                            <input type="file" class="form-control" name="archivoimagen">
+                        </div>
+                        <div class="form-group">
+                            <label for="archivopdf">Archivo PDF: </label>
+                            <input type="file" class="form-control" name="archivopdf">
                         </div>
                         <div class="form-group">        
                             <input name="submit" type="submit" value="Subir" class="btn btn-success"></input>
@@ -169,4 +144,4 @@
 
         ga('create', 'UA-38548366-1', 'auto');
         ga('send', 'pageview');
-    </script>
+</script>
